defa2fbcf05c6b1523b399354c2bf9bbbd3dc2ed
Niclas van Eyk <niclas.eyk@gmail.com>
2023-10-15 18:04:26 +0200

    add @vercel/analytics

--- chers_web/app/layout.tsx
+++ chers_web/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -24,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }

f510206af3cc8336cfbf455582e4a37eb7b531a1
Niclas van Eyk <niclas.eyk@gmail.com>
2023-10-15 17:59:29 +0200

    more metadata

--- chers_web/app/layout.tsx
+++ chers_web/app/layout.tsx
@@ -6,8 +6,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Chers",
+  applicationName: "Chers",
   description: "A rusty chess implementation for the terminal and web.",
   manifest: "manifest.json",
+  creator: "Niclas van Eyk",
+  keywords: "Chess, Rust, Webassembly",
+  alternates: {
+    canonical: "https://chers.niclasve.me",
+  },
 };
 
 export default function RootLayout({

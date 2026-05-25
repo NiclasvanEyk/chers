38af2ff2d6b4f9fde91a2f222ea74b0e404043e8
Niclas van Eyk <niclas.eyk@gmail.com>
2026-05-25 23:13:47 +0200

    remove the semi-functional chers-cli remote stuff
    This will be done via a unified mechanism in the future, which hopefully
    re-uses infra from chers-server

--- chers_cli/src/main.rs
+++ chers_cli/src/main.rs
@@ -1,64 +1,19 @@
 use std::process::exit;
 
 use clap::Parser;
-
 use chers_cli::modes::local::TerminalChersMatch;
-use chers_cli::modes::remote::connection::Role;
-use chers_cli::modes::remote::game::RemoteChersMatch;
-use chers_cli::moves::serialization::SimpleMoveConverter;
-use chers_cli::moves::transport::Coordinator;
 
 #[derive(Parser)]
 #[command(name = "chers")]
 #[command(version = "0.0.1-alpha")]
 #[command(about = "Play chess on your terminal")]
-struct Cli {
-    /// When present, starts a remote game.
-    ///
-    /// If absent, a local game is started, where white and black play on the same terminal.
-    role: Option<Role>,
-
-    /// The host to connect to or host from, dependending on the role.
-    ///
-    /// This is only relevant for remote games. Default: '127.0.0.1' (localhost).
-    #[arg(long)]
-    host: Option<String>,
-
-    /// The port to connect to or host from, dependending on the role.
-    #[arg(long)]
-    port: Option<u32>,
-}
+struct Cli;
 
 fn main() {
-    let cli = Cli::parse();
+    Cli::parse();
 
-    let Some(role) = cli.role else {
-        let mut ui = TerminalChersMatch::new();
-
-        ui.run();
-        exit(0);
-    };
-
-    let maybe_stream = match role.connect(cli.host, cli.port) {
-        Ok(x) => x,
-        Err(error) => {
-            println!(
-                "Something went wrong while trying to establish a connection: {:?}",
-                error
-            );
-            exit(1);
-        }
-    };
-    let Some(stream) = maybe_stream else {
-        println!("That did not work. Maybe try again?");
-        exit(1);
-    };
-
-    let other = stream.peer_addr().unwrap().to_string();
-    println!("Successfully connected to {other}!");
-
-    let coordinator = Coordinator::new(stream, Box::new(SimpleMoveConverter::new()));
-    let mut ui = RemoteChersMatch::new(coordinator);
+    let mut ui = TerminalChersMatch::new();
 
     ui.run();
+    exit(0);
 }
--- chers_cli/src/modes/mod.rs
+++ chers_cli/src/modes/mod.rs
@@ -1,7 +1,1 @@
-/// A locally played game, where both players input their moves separately.
 pub mod local;
-
-/// A remotely played game over a TCP connection.
-///
-/// Players take turns and one has to wait for each others moves.
-pub mod remote;
